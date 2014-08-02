5ff42581d29df53f7f049ca1b3867a4506d56fd1
Deepak Rai <mail2deepkarai@gmail.com>
2014-08-02 18:23:43 +0530

    Items Model

--- application/views/company/addAd.php
+++ application/views/company/addAd.php
@@ -1,8 +1,10 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>templates/admin/css/jquery-ui.css">
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
+
 <script type='text/javascript' src='//code.jquery.com/ui/1.10.4/jquery-ui.js?ver=2013-07-18'></script>
 <script src="<?php echo base_url(); ?>templates/admin/js/bootstrap-tagsinput.min.js" type="text/javascript"></script>
-                
+
+<script type="text/javascript" src='http://maps.google.com/maps/api/js?sensor=false&libraries=places'></script>
+<script src="<?php echo base_url(); ?>templates/front/js/locationpicker.jquery.js" type="text/javascript"></script>                
 <style>
 .dataTables_filter
 {
@@ -12,31 +14,30 @@
 
  <script type="text/javascript">// <![CDATA[
  $(document).ready(function(){
-	 $('#category').change(function(){ 
-	 $("#items > option").remove();
-	 var category_id = $('#category').val();
+	 $('#category').change(function(){ //any select change on the dropdown with id country trigger this code
+	 $("#items > option").remove(); //first of all clear select items
+	 var category_id = $('#category').val(); // here we are taking country id of the selected one.
 	 $.ajax({
 	 type: "POST",
-	 url: "<?php echo site_url('company/getitems'); ?>/"+category_id, 
+	 url: "<?php echo site_url('company/get_items'); ?>/"+category_id, //here we are calling our user controller and get_cities method with the country_id
 	 
-	 success: function(items)
+	 success: function(items) //we're calling the response json array 'cities'
 	 {
-	 $.each(items,function(id,myItems)
+	 $.each(items,function(id,myItems) //here we're doing a foeach loop round each city with id as the key and city as the value
 	 {
-		 var opt = $('<option />');
+		 var opt = $('<option />'); // here we're creating a new select option with for each city
 		 opt.val(id);
 		 opt.text(myItems);
-		 $('#items').append(opt);
+		 $('#items').append(opt); //here we will append these new select options to a dropdown with the id 'cities'
 	 });
 	 }
+	 
 	 });
+	 
 	 });
  });
  // ]]>
 </script>
-
-
-
 
     <div class="content">  
     	<?php echo $this->session->flashdata('message'); ?>
@@ -58,7 +59,7 @@
                 <a class="btn btn-green" href="<?php echo site_url('company/ads'); ?>">&lt;&lt; Back</a>
                 <br/>
                 <form class="" method="post" action="<?php echo base_url("company/saveAd"); ?>" enctype="multipart/form-data">
-                <div  style="width:48%; float:left;">
+                <div  style="width:100%; float:left;">
          
                     <br/>
 
@@ -103,137 +104,36 @@
                         </div>
                     </div>
                     
+             
+ 
                    
-                     <div id="map-container">
                         <label class="">Address</label>
                         <div class="">
-                            <input type="text" id="address" name="address" class="span10" value="">
+                            <input type="text" id="address" name="address" class="span10" value="" autocomplete="off">
                             <p class="help-block">Start typing an address and select from the dropdown.</p>
-
+							
+                            <div id="map-container" style="float:right;">
 						    <div id="map-canvas"></div>
 
-						    <script type="text/javascript">
-
-								jQuery(document).ready(function($) {
-
-									var geocoder;
-									var map;
-									var marker;
-
-									var geocoder = new google.maps.Geocoder();
-
-									function geocodePosition(pos) {
-									  geocoder.geocode({
-									    latLng: pos
-									  }, function(responses) {
-									    if (responses && responses.length > 0) {
-									      updateMarkerAddress(responses[0].formatted_address);
-									    } else {
-									      updateMarkerAddress('Cannot determine address at this location.');
-									    }
-									  });
-									}
-
-									function updateMarkerPosition(latLng) {
-									  jQuery('#latitude').val(latLng.lat());
-									  jQuery('#longitude').val(latLng.lng());
-									}
-
-									function updateMarkerAddress(str) {
-									  jQuery('#address').val(str);
-									}
-
-									function initialize() {
-
-									  var latlng = new google.maps.LatLng(0, 0);
-									  var mapOptions = {
-									    zoom: 2,
-									    center: latlng
-									  }
-
-									  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-									  geocoder = new google.maps.Geocoder();
-
-									  marker = new google.maps.Marker({
-									  	position: latlng,
-									    map: map,
-									    draggable: true
-									  });
-
-									  // Add dragging event listeners.
-									  google.maps.event.addListener(marker, 'dragstart', function() {
-									    updateMarkerAddress('Dragging...');
-									  });
-									  
-									  google.maps.event.addListener(marker, 'drag', function() {
-									    updateMarkerPosition(marker.getPosition());
-									  });
-									  
-									  google.maps.event.addListener(marker, 'dragend', function() {
-									    geocodePosition(marker.getPosition());
-									  });
-
-									}
-
-									google.maps.event.addDomListener(window, 'load', initialize);
-
-									jQuery(document).ready(function() { 
-									         
-									  initialize();
-									          
-									  jQuery(function() {
-									    jQuery("#address").autocomplete({
-									      //This bit uses the geocoder to fetch address values
-									      source: function(request, response) {
-									        geocoder.geocode( {'address': request.term }, function(results, status) {
-									          response(jQuery.map(results, function(item) {
-									            return {
-									              label:  item.formatted_address,
-									              value: item.formatted_address,
-									              latitude: item.geometry.location.lat(),
-									              longitude: item.geometry.location.lng()
-									            }
-									          }));
-									        })
-									      },
-									      //This bit is executed upon selection of an address
-									      select: function(event, ui) {
-									        jQuery("#latitude").val(ui.item.latitude);
-									        jQuery("#longitude").val(ui.item.longitude);
-
-									        var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
-
-									        marker.setPosition(location);
-									        map.setZoom(16);
-									        map.setCenter(location);
-
-									      }
-									    });
-									  });
-									  
-									  //Add listener to marker for reverse geocoding
-									  google.maps.event.addListener(marker, 'drag', function() {
-									    geocoder.geocode({'latLng': marker.getPosition()}, function(results, status) {
-									      if (status == google.maps.GeocoderStatus.OK) {
-									        if (results[0]) {
-									          jQuery('#address').val(results[0].formatted_address);
-									          jQuery('#latitude').val(marker.getPosition().lat());
-									          jQuery('#longitude').val(marker.getPosition().lng());
-									        }
-									      }
-									    });
-									  });
-									  
-									});
-
-								});
-
-						    </script>
+							<script>
+                                $('#map-canvas').locationpicker({
+                                location: {latitude: 46.15242437752303, longitude: 2.7470703125},	
+                                radius: 300,
+                                inputBinding: {
+                                    latitudeInput: $('#latitude'),
+                                    longitudeInput: $('#longitude'),
+                                    locationNameInput: $('#address')        
+                                },
+                                enableAutocomplete: true,
+                                onchanged: function(currentLocation, radius, isMarkerDropped) {
+                                    alert("Location changed. New location (" + currentLocation.latitude + ", " + currentLocation.longitude + ")");
+                                }	
+                                });
+							</script>
                         </div>
-                        </div>
-                   
-                    
+                   </div>
+         
+             
                     <div class="control-group">
                         <label class="control-label">Latitude</label>
                         <div class="controls">
@@ -248,6 +148,9 @@
                             
                         </div>
                     </div>
+          
+                    
+       
 
                     <div class="control-group">
                         <label class="control-label">File</label>
