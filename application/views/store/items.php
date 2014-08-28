81f8cfbc921567a01a83fd68782ed57264da1e54
xecom <tushar.autkar@xecomit.com>
2014-08-28 19:30:01 +0530

    Point: 28August2014 2nd revision comitted new files added

--- application/views/store/items.php
+++ application/views/store/items.php
@@ -237,8 +237,6 @@ $(document).ready(function() {
 
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
 <script>
 $(function() {
 $( document ).tooltip();
