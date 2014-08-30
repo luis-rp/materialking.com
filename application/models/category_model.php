0d86e5aa66ba367fa4d146774e28215a39b0430b
luis-rp <syd.sozo@gmail.com>
2014-08-29 17:34:38 -0700

    working on dropbox tasks2

---
+++ application/models/category_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class category_model extends Model {
+
+    private $search;
+    private $keyword;
+
+    function category_model() {
+        parent::Model();
+    }
+
+
+ 
+//function get_countries() {}
+ 
+    
+}
---
+++ application/views/company/formbuilderselector.php
@@ -0,0 +1,32 @@
+<script type="text/javascript">
+function showOptions(obj)
+{
+	var fieldType = $(obj).val();
+
+	if(fieldType == 'radio' || fieldType == 'dropdown'|| fieldType == 'checkbox')
+	{
+		$("#optionDiv").css("display","block");
+	}
+}
+</script>
+<div class="content">
+
+  <div class="container">
+  	<div class="row">
+  	  <div class="col-md-12">
+         <div class="grid simple ">
+            <div class="grid-title no-border">
+              <?php  if(isset($message)) echo $message; else echo ''; ?>
+            
+            </div>
+
+                  <div class="grid-body no-border">
+                   <div class="row">
+            			<div><a href="<?php echo base_url() . 'company/createformnetwork';?>"><input type="button"  class="btn btn-primary" value="Manage Network Request Requirements"></a></div>
+            			<br><br><br><br><br>
+						<div><a href="<?php echo base_url() . 'company/createformsubscriptions';?>"><input type="button"  class="btn btn-primary" value="Manage Newsletter Request Requirements"></a></div>
+                 </div>
+              </div>
+          </div>
+  		</div>
+	</div>
