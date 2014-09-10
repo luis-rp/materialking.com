38e559d96a648d0738ad853a61094099ad296c51
xecom <tushar.autkar@xecomit.com>
2014-09-10 19:53:36 +0530

    Point: 10Sept2014 2nd revision comitted new files added

--- application/views/admin/report.php
+++ application/views/admin/report.php
@@ -124,7 +124,7 @@ $(document).ready(function(){
 			    		<td><?php echo $item->costcode;?></td>
 			    		<td><?php echo $item->notes;?></td>
 			    		<td><?php echo $item->invoicenum;?></td>
-			    		<td><?php  if(isset($item->datedue) && $item->datedue!="") { echo date("m/d/Y", strtotime($item->datedue)); } else { echo ""; }  ?></td>
+			    		<td><?php  if(isset($item->datedue) && $item->datedue!="") { echo date("m/d/Y", strtotime($item->datedue)); } else { echo "No Date Set"; }  ?></td>
 			    		<?php if(0){?>
 			    		<td>
 			    		<?php if($item->paymentstatus){?>
